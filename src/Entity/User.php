cd9970f705438a2090bbefaba112410b47efbea6
Thierry Thuon <thierry@thuon.eu>
2020-02-23 14:23:00 +0100

    create user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,30 +1,72 @@
 <?php
-declare(strict_types=1);
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\UuidInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
 class User implements UserInterface
 {
-    public function getRoles()
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="uuid_binary_ordered_time", unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+     */
+    private UuidInterface $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private string $username;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    public function getId(): ?UuidInterface
     {
-        return ['ROLE_USER'];
+        return $this->id;
     }
 
-    public function getPassword()
+    public function getUsername(): string
+    {
+        return (string) $this->username;
+    }
+
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): void
+    {
+        $this->roles = $roles;
+    }
+
+    public function getPassword(): void
     {
     }
 
-    public function getSalt()
+    public function getSalt(): void
     {
     }
 
-    public function getUsername()
-    {
-    }
-
-    public function eraseCredentials()
+    public function eraseCredentials(): void
     {
     }
 }
