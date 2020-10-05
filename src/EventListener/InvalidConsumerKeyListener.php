25d86b7b2d81551a6026039ca3a52b8620ef2ea2
Thierry T <1940947+lepiaf@users.noreply.github.com>
2020-10-06 00:48:31 +0200

    catch exception on consumer key invalide

--- src/EventListener/InvalidConsumerKeyListener.php
+++ src/EventListener/InvalidConsumerKeyListener.php
@@ -2,20 +2,25 @@
 
 namespace App\EventListener;
 
+use App\Exception\ConsumerKeyNotFoundInSessionException;
 use GuzzleHttp\Exception\ClientException;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 
-class OvhForbiddenException
+class InvalidConsumerKeyListener
 {
     public function onKernelException(ExceptionEvent $event)
     {
         $exception = $event->getThrowable();
+        $response = new RedirectResponse('/login/provider/ovh');
         if ($exception instanceof ClientException) {
             if ($exception->getCode() === 403) {
-                $response = new RedirectResponse('/login/provider/ovh');
                 $event->setResponse($response);
             }
+        }
+
+        if ($exception instanceof ConsumerKeyNotFoundInSessionException) {
+            $event->setResponse($response);
         }
     }
 }
